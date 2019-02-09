b85be6a9d212cd6db4aa3d86e64643a130b724bc
Scott Joseph Emrich <semrich@tesla1.eecs.utk.edu>
2019-02-08 20:50:56 -0500

    challenge04 sample

---
+++ challenge04/solution.cpp
@@ -0,0 +1,16 @@
+// Challenge 04: kth smallest numbers
+// Name:  
+
+// Brief description:
+
+// This code reads in a number of ints...
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+}
